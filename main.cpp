259af147e2589dfb3d724b521c653a648b0a6617
Roman Glaz <70313579+Vokerlee@users.noreply.github.com>
2021-04-10 01:48:06 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@ int main()
 {
     window manager(850, 550, "Paint");
 
-    manager.add_button(new button(100, 50, 620, 430, 255, 255, 255));
-    manager.add_button(new button(755, 100, 25, 25, 255, 0, 0));
-    manager.add_button(new button(755, 140, 25, 25, 0, 255, 0));
-    manager.add_button(new button(755, 180, 25, 25, 0, 0, 255));
-    manager.add_button(new button(755, 220, 25, 25, 0, 255, 255));
-    manager.add_button(new button(755, 260, 25, 25, 255, 0, 255));
-    manager.add_button(new button(755, 300, 25, 25, 255, 255, 0));
+    manager.add_tool(new ellipse(30, 50, 20, 10, 100, 55, 55));
+    manager.add_tool(new canvas(100, 50, 620, 430, 255, 255, 255));
+    manager.add_tool(new colour_button(755, 100, 25, 25, 255, 0, 0));
+    manager.add_tool(new colour_button(755, 140, 25, 25, 0, 255, 0));
+    manager.add_tool(new colour_button(755, 180, 25, 25, 0, 0, 255));
+    manager.add_tool(new colour_button(755, 220, 25, 25, 0, 255, 255));
+    manager.add_tool(new colour_button(755, 260, 25, 25, 255, 0, 255));
+    manager.add_tool(new colour_button(755, 300, 25, 25, 255, 255, 0));
 
     manager.run();
 
